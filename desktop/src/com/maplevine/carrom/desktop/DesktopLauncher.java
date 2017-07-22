2a0c537dde3b19d4294e8dbe95acc2db15be888a
Jeremy (Jeung Hwan) Suh <jeunghwansuh@gmail.com>
2017-07-23 04:08:58 +0700

    Added standard files

---
+++ desktop/src/com/maplevine/carrom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maplevine.carrom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maplevine.carrom.Carrom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Carrom(), config);
+	}
+}
